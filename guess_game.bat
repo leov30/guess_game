f575ad43166a8db42f99556f4e76432d53a13a6c
leov30 <leo_v30@hotmail.com>
2024-07-16 11:03:35 -0700

    Update guess_game.bat
    added fix for database not having double quotes

--- guess_game.bat
+++ guess_game.bat
@@ -9,10 +9,15 @@ for /f "delims=" %%g in (riddles.csv) do set /a _lines+=1
 set /a _lines-=1
 
 :loop
-cls&title Points: %_point% Lives: %_lives%
-
 call :get_question
-set "_answ=%_answ:~1%"
+
+if "%_answ%"=="" (
+	goto :loop
+)else (
+	set "_answ=%_answ:~1%"
+)
+
+title Points: %_point% Lives: %_lives%
 
 echo:
 set /p "_guess=Guess? "
@@ -32,7 +37,8 @@ if %_lives% gtr 0 (
 )
 
 :get_question
-
+cls
+set "_answ="
 set /a _rand=(%random%%%(%_lines%-1+1))+1
 
 for /f skip^=%_rand%^ tokens^=1^,2^ delims^=^" %%g in (riddles.csv) do (
@@ -40,4 +46,3 @@ for /f skip^=%_rand%^ tokens^=1^,2^ delims^=^" %%g in (riddles.csv) do (
 	set "_answ=%%h"
 	exit /b
 )
-
